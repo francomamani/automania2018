d978192c7ea7661cc059399f43d5517ddd0f8fc2
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2020-06-18 11:01:03 -0400

    guv update server

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  base: 'http://54.85.218.91/automania2018/backend/public/api/',
+  base: 'https://gotasoft.com/automania2018/backend/public/api/',
+  base3: 'http://54.85.218.91/automania2018/backend/public/api/',
   base2: 'http://localhost:8000/api/'
 };
