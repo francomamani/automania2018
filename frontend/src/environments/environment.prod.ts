916f6016cacc64d930fdbd7499ed1eae8f55d19f
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2020-06-18 10:53:16 -0400

    sistema de gesion de gasolina

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base2: 'http://54.85.218.91/automania2018/backend/public/api/',
-  base: 'http://localhost:8000/api/'
+  base: 'http://54.85.218.91/automania2018/backend/public/api/',
+  base2: 'http://localhost:8000/api/'
 };
